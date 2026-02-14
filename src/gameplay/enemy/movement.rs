486ba89f712eb34fd658fdcf23842d5c87322a24
Augustin Gjini <augustin.gjini@pm.me>
2026-02-14 15:05:02 +0100

    fix wasm

--- src/gameplay/enemy/movement.rs
+++ src/gameplay/enemy/movement.rs
@@ -1,10 +1,18 @@
 use crate::gameplay::enemy::asset::Enemy;
 use crate::gameplay::movement::MovementController;
 use crate::gameplay::player::Player;
+use crate::{AppSystems, PausableSystems};
 use bevy::prelude::*;
 
 pub fn plugin(app: &mut App) {
-    app.add_systems(Update, move_enemies);
+    app.add_systems(
+        Update,
+        (
+            update_move_timer.in_set(AppSystems::TickTimers),
+            (move_enemies,).chain().in_set(AppSystems::Update),
+        )
+            .in_set(PausableSystems),
+    );
     app.insert_resource(EnemyMoveTimer::default());
 }
 
@@ -17,13 +25,15 @@ impl Default for EnemyMoveTimer {
     }
 }
 
+fn update_move_timer(time: Res<Time>, mut timer: ResMut<EnemyMoveTimer>) {
+    timer.0.tick(time.delta());
+}
+
 fn move_enemies(
-    time: Res<Time>,
-    mut timer: ResMut<EnemyMoveTimer>,
+    timer: Res<EnemyMoveTimer>,
     enemies: Query<(&Transform, &mut MovementController), With<Enemy>>,
     player: Single<&Transform, With<Player>>,
 ) {
-    timer.0.tick(time.delta());
     if !timer.0.just_finished() {
         return;
     }
